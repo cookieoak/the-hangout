12b7b9274e260be7b173b5817babaa5ad0a54e5b
cookieoak <disneyguide1955@gmail.com>
2025-02-26 14:23:31 -0800

    server.js

--- server.js
+++ server.js
@@ -36,3 +36,36 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+const io = require('socket.io')(server);
+
+let gameState = Array(9).fill(null);
+let currentPlayer = 'X';
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    
+    socket.on('playerMove', (data) => {
+        if (gameState[data.index] === null) {
+            gameState[data.index] = data.player;
+            io.emit('opponentMove', data);
+            checkForWinner();
+            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+        }
+    });
+
+    function checkForWinner() {
+        const winningCombinations = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],
+            [0, 4, 8], [2, 4, 6]
+        ];
+
+        winningCombinations.forEach(combination => {
+            const [a, b, c] = combination;
+            if (gameState[a] && gameState[a] === gameState[b] && gameState[a] === gameState[c]) {
+                io.emit('gameOver', `${gameState[a]} wins!`);
+                gameState = Array(9).fill(null);
+            }
+        });
+    }
+});
